cf2ff59bff3e5dc00cbe713ddbe9af08bbb6b7f5
sage-jordan <sagemjordan@gmail.com>
2020-02-10 22:58:54 -0500

    setup dotenv and PR req's checked

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 4000;
 const server = require("./api/server");
+require("dotenv").config();
 
 server.listen(port, () => {
   console.log(`\n === Server listening on port ${port} === \n`);
