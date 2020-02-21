7415aace3b6ffba6cad171b259bd5fa0933db81f
sage-jordan <sagemjordan@gmail.com>
2020-02-20 22:18:53 -0500

    Updated Server
    Added server comments and setup routes imports and server.use
    
    Co-Authored-By: Darren Carrillo <darrenjcarrillo@users.noreply.github.com>

--- api/server/server.js
+++ api/server/server.js
@@ -1,13 +1,31 @@
+// Server Imports
 const express = require("express");
 const middleware = require("./middleware");
 
-const server = express();
+// Router Imports
+const userRouter = require("../routers/01-users");
+const propertiesRouter = require("../routers/02-properties");
+const leaseTermsRouter = require("../routers/03-leaseterms");
+const unitsRouter = require("../routers/04-units");
+const workOrdersRouter = require("../routers/05-workorders");
+const paymentsRouter = require("../routers/06-payments");
 
+// Server Setup
+const server = express();
 middleware(server);
 
+// Simple GET request
 server.get("/", (req, res) => {
   console.log("It's alive!");
   res.status(200).json({ message: "It's alive!" });
 });
+
+// Router Setup
+server.use("/users", userRouter);
+server.use("/properties", propertiesRouter);
+server.use("/lease-terms", leaseTermsRouter);
+server.use("/units", unitsRouter);
+server.use("/work-orders", workOrdersRouter);
+server.use("/payments", paymentsRouter);
 
 module.exports = server;
