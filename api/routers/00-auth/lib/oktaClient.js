fe7056876b780dc8ec1127c4071ef10a674155b2
Darren Carrillo <48144231+darrenjcarrillo@users.noreply.github.com>
2020-02-26 19:06:30 -0800

    Lease term bug fixed
    Fixed bug on lease terms
    
    Co-Authored-By: Sage Jordan <sage-jordan@users.noreply.github.com>
    Co-Authored-By: etridgely <etridgely@users.noreply.github.com>

--- api/routers/00-auth/lib/oktaClient.js
+++ api/routers/00-auth/lib/oktaClient.js
@@ -1,8 +1,8 @@
 const okta = require("@okta/okta-sdk-nodejs");
 
 const client = new okta.Client({
-  orgUrl: "https://{dev-600562.okta.com}",
-  token: "{00LGWkCOoKWi0IAUnn9Trcbn1XP1YEYAZBEVeoch5h}"
+  orgUrl: "https://dev-600562.okta.com",
+  token: "00LGWkCOoKWi0IAUnn9Trcbn1XP1YEYAZBEVeoch5h"
 });
 
 module.exports = client;
