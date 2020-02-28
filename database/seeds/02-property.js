cc3a862432bb8a038c5d2d80fedd1d010e31a738
sage-jordan <sagemjordan@gmail.com>
2020-02-27 23:18:10 -0500

    Property seeds
    Co-Authored-By: etridgely <etridgely@users.noreply.github.com>
    Co-Authored-By: Darren Carrillo <darrenjcarrillo@users.noreply.github.com>

--- database/seeds/02-property.js
+++ database/seeds/02-property.js
@@ -15,7 +15,36 @@ exports.seed = function(knex) {
           name: "Slums",
           img:
             "https://www.habitatforhumanity.org.uk/wp-content/uploads/2017/10/what-is-a-slum-definition-in-romania.jpg",
-          manager_id: 2
+          manager_id: 1
+        },
+        {
+          name: "123 Fake Street",
+          img: "https://frinkiac.com/img/S12E18/1067483.jpg",
+          manager_id: 1
+        },
+        {
+          name: "4934 Charter Way",
+          img:
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.washingtonian.com%2F2019%2F05%2F02%2Fthe-five-best-looking-open-houses-this-weekend-5-4-5-5%2F&psig=AOvVaw2Y2E001EJpSPMt45E5BoUj&ust=1582949747766000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJDr3b-x8-cCFQAAAAAdAAAAABAJ",
+          manager_id: 3
+        },
+        {
+          name: "Lambda Headquarters",
+          img:
+            "https://upload.wikimedia.org/wikipedia/en/4/49/Lambdaschoolofmusicandfinearts.png",
+          manager_id: 1
+        },
+        {
+          name: "Fancy House",
+          img:
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcollegiannews.com%2F10-most-common-types-of-houses-in-the-usa%2F507137%2F&psig=AOvVaw2Y2E001EJpSPMt45E5BoUj&ust=1582949747766000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJDr3b-x8-cCFQAAAAAdAAAAABAa",
+          manager_id: 1
+        },
+        {
+          name: "The Hobbit House",
+          img:
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_130522681_matamata-new-zealand-october-10-2018-hobbit-house-hobbiton-movie-set.html&psig=AOvVaw1Ea7JvSUfyf5f4ZkVEeRa2&ust=1582949838808000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCGze2x8-cCFQAAAAAdAAAAABAD",
+          manager_id: 3
         }
       ]);
     });
