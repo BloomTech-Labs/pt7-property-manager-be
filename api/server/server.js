e8ed42a57cd3931cf64ffe63eaa283c9018ade7b
sage-jordan <sagemjordan@gmail.com>
2020-02-27 22:57:10 -0500

    Server.js
    simplified to authRouter, userRouter, and propertiesRouter, and fixed userRouter
    
    Co-Authored-By: etridgely <etridgely@users.noreply.github.com>
    Co-Authored-By: Darren Carrillo <darrenjcarrillo@users.noreply.github.com>

--- api/server/server.js
+++ api/server/server.js
@@ -6,10 +6,6 @@ const middleware = require("./middleware");
 const authRouter = require("../routers/00-auth/auth-router");
 const userRouter = require("../routers/01-users/users-router");
 const propertiesRouter = require("../routers/02-properties/properties-router");
-const leaseTermsRouter = require("../routers/03-leaseterms/leaseterms-router");
-const unitsRouter = require("../routers/04-units/units-router");
-const workOrdersRouter = require("../routers/05-workorders/workorders-router");
-const paymentsRouter = require("../routers/06-payments/payments-router");
 
 // Server Setup
 const server = express();
@@ -23,11 +19,7 @@ server.get("/", (req, res) => {
 
 // Router Setup
 server.use("/auth", authRouter);
-server.use("/users", authRouter);
+server.use("/users", userRouter);
 server.use("/properties", propertiesRouter);
-// server.use("/lease-terms", leaseTermsRouter);
-// server.use("/units", unitsRouter);
-// server.use("/work-orders", workOrdersRouter);
-// server.use("/payments", paymentsRouter);
 
 module.exports = server;
