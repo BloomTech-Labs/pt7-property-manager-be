fe7faabba3a254d0badc17b091dcc08bd291f3d4
sage-jordan <sagemjordan@gmail.com>
2020-03-09 22:13:24 -0400

    PUT bug
    Fixed secret in middleware file, updated users model and router to make it so you don't have to submit a full user obj to update.
    
    Co-Authored-By: Darren Carrillo <darrenjcarrillo@users.noreply.github.com>
    Co-Authored-By: etridgely <etridgely@users.noreply.github.com>

--- api/routers/01-users/users-router.js
+++ api/routers/01-users/users-router.js
@@ -59,22 +59,41 @@ router.put("/:id", authenticate, (req, res) => {
   // Updates user by
   const id = req.params.id;
   const user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10);
-  user.password = hash;
 
-  User.updateUser(user, id)
-    .then(updated => {
-      if (updated) {
-        res.status(200).json({ updated });
-      } else {
-        res.status(400).json({ message: "Please provide a valid id" });
-      }
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ err: err.message, message: "Error updating Manager" });
-    });
+  // If password, hash it
+  if (user.password) {
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    User.updateUser(user, id)
+      .then(updated => {
+        if (updated) {
+          res.status(200).json({ updated });
+        } else {
+          res.status(400).json({ message: "Please provide a valid id" });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ err: err.message, message: "Error updating Manager" });
+      });
+    // If no password, continue to update
+  } else {
+    User.updateUser(user, id)
+      .then(updated => {
+        if (updated) {
+          res.status(200).json({ updated });
+        } else {
+          res.status(400).json({ message: "Please provide a valid id" });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ err: err.message, message: "Error updating Manager" });
+      });
+  }
 });
 
 router.delete("/:id", authenticate, (req, res) => {
