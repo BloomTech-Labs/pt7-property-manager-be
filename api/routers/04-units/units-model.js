acf08a4621cc87036e3e52f631ea2d2d7c03352d
Darren Carrillo <48144231+darrenjcarrillo@users.noreply.github.com>
2020-04-08 19:33:37 -0700

    Updated units end points
    Added fields to units request,
    Fixed environment bug
    Manager able to edit renter
    
    Co-Authored-By: Sage Jordan <sage-jordan@users.noreply.github.com>
    Co-Authored-By: etridgely <etridgely@users.noreply.github.com>

--- api/routers/04-units/units-model.js
+++ api/routers/04-units/units-model.js
@@ -16,9 +16,6 @@ function addUnit(unit) {
 
 function findAllUnits() {
   return db("units")
-    .join("users as u", "units.renter_id", "=", "u.id")
-    .join("leaseterms as l", "units.lease_id", "=", "l.id")
-    .join("property as p", "units.property_id", "=", "p.id")
     .select(
       "units.id",
       "number",
@@ -44,11 +41,48 @@ function findAllUnits() {
       "zip",
       "country",
       "manager_id"
-    );
+    )
+    .join("users as u", "units.renter_id", "=", "u.id")
+    .join("leaseterms as l", "units.lease_id", "=", "l.id")
+    .join("property as p", "units.property_id", "=", "p.id");
 }
 
 function findUnitById(id) {
   return db("units")
+    .select(
+      "units.id",
+      "number",
+      "property_id",
+      "description",
+      "date_available",
+      "parking",
+      "type",
+      "cooling",
+      "heating",
+      "pets",
+      "laundry",
+      "fees",
+      "sqft",
+      "elementary",
+      "middle",
+      "high",
+      "district",
+      "p.img",
+      "address",
+      "city",
+      "state",
+      "zip",
+      "country",
+      "manager_id",
+      "payment_due_date",
+      "lease_start_date",
+      "lease_end_date",
+      "lease_term",
+      "monthly_rent",
+      "security_deposit",
+      "firstName",
+      "lastName"
+    )
     .join("users as u", "units.renter_id", "=", "u.id")
     .join("leaseterms as l", "units.lease_id", "=", "l.id")
     .join("property as p", "units.property_id", "=", "p.id")
