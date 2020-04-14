de0e576cb9434e8855edb286cb043232a3de163f
Darren Carrillo <48144231+darrenjcarrillo@users.noreply.github.com>
2020-04-13 19:23:09 -0700

    Updated the following files for code maintainability
    Updated the following files for code maintainability
    
    Co-Authored-By: Sage Jordan <sage-jordan@users.noreply.github.com>
    Co-Authored-By: etridgely <etridgely@users.noreply.github.com>

--- database/seeds/06-payments.js
+++ database/seeds/06-payments.js
@@ -1,3 +1,4 @@
+// prettier-ignore
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("payments")
@@ -5,21 +6,9 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("payments").insert([
-        {
-          payment_type: "cash",
-          payment_amount: 1000,
-          payment_date: "2020-02-11",
-          reference_number: 1,
-          payment_category: "rent",
-          lease_id: 1
+        {payment_type: "cash",payment_amount: 1000,payment_date: "2020-02-11",reference_number: 1,payment_category: "rent",lease_id: 1
         },
-        {
-          payment_type: "cash",
-          payment_amount: 1000,
-          payment_date: "2020-02-11",
-          reference_number: 2,
-          payment_category: "rent",
-          lease_id: 2
+        {payment_type: "cash",payment_amount: 1000,payment_date: "2020-02-11",reference_number: 2,payment_category: "rent",lease_id: 2
         }
       ]);
     });
