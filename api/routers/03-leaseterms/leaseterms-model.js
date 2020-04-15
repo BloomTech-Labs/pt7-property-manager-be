4c8e7ffbc80ae95505e53e31eb7d8856f04bc6de
sage-jordan <sagemjordan@gmail.com>
2020-04-14 21:24:06 -0400

    fixed leaseterms model and setup leaseterms and applications endpoints in server
    Co-Authored-By: Darren Carrillo <darrenjcarrillo@users.noreply.github.com>
    Co-Authored-By: etridgely <etridgely@users.noreply.github.com>

--- api/routers/03-leaseterms/leaseterms-model.js
+++ api/routers/03-leaseterms/leaseterms-model.js
@@ -1,37 +1,31 @@
 const db = require("../../../database/db-config.js");
 
 module.exports = {
-    addLeaseTerm, // Adds a lease term
-    findAllLeaseTerm, // Gets all lease terms
-    findLeaseTermById, // Find a lease term
-    updateLeaseTerm, // Updates a lease term
-    removeLeaseTerm // Deletes a lease term
-}
+  addLeaseTerm,
+  findAllLeaseTerm,
+  findLeaseTermById,
+  updateLeaseTerm,
+  removeLeaseTerm,
+};
 
-function addLeaseTerm(lease_term) {
-    return db("lease_term")
-        .insert(lease_term, "id")
-        .then( ids => ({ id: ids[0]}));
+function addLeaseTerm(leaseterms) {
+  return db("leaseterms")
+    .insert(leaseterms, "id")
+    .then((ids) => ({ id: ids[0] }));
 }
 
 function findAllLeaseTerm() {
-    return db("lease_term")
+  return db("leaseterms");
 }
 
 function findLeaseTermById(id) {
-    return db("lease_term")
-        .where({ id })
-        .first();
+  return db("leaseterms").where({ id }).first();
 }
 
 function updateLeaseTerm(changes, id) {
-    return db("lease_term")
-        .where({ id })
-        .update(changes)
+  return db("leaseterms").where({ id }).update(changes);
 }
 
 function removeLeaseTerm(id) {
-    return db("lease_term")
-        .where({ id })
-        .delete();
+  return db("leaseterms").where({ id }).delete();
 }
