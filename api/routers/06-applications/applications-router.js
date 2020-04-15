c20aed26391e72e56db0df4f95f8ac15d12d9b06
sage-jordan <sagemjordan@gmail.com>
2020-04-14 21:14:22 -0400

    Applications router and some simplifications/fixes
    Wrote applications router and model, deleted empty payments router and model, fixed a few errors in least terms router
    
    Co-Authored-By: Darren Carrillo <darrenjcarrillo@users.noreply.github.com>
    Co-Authored-By: etridgely <etridgely@users.noreply.github.com>

--- api/routers/06-applications/applications-router.js
+++ api/routers/06-applications/applications-router.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const lease_term = require("./leaseterms-model");
+const applications = require("./applications-model");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  const lease = req.body;
-  lease_term
-    .addLeaseTerm(lease)
-    .then((term) => {
-      res.status(200).json({ term });
+  const app = req.body;
+  applications
+    .addApp(app)
+    .then((application) => {
+      res.status(200).json({ application });
     })
     .catch((err) => {
       res.status(500).json({ err: err.message });
@@ -15,25 +15,25 @@ router.post("/", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  lease_term
-    .findAllLeaseTerm()
-    .then((lease_term) => {
-      res.status(200).json({ lease_term });
+  applications
+    .findAllApps()
+    .then((applications) => {
+      res.status(200).json({ applications });
     })
     .catch((err) =>
       res
         .status(500)
-        .json({ error: "Failed to get all leases", err: err.message })
+        .json({ error: "Failed to get all applications", err: err.message })
     );
 });
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  lease_term
-    .findLeaseTermById(id)
-    .then((lease_term) => {
-      if (lease_term) {
-        res.status(200).json({ lease_term });
+  applications
+    .findAppById(id)
+    .then((application) => {
+      if (application) {
+        res.status(200).json({ application });
       } else {
         res.status(400).json({ message: "Please supply a valid id" });
       }
@@ -41,15 +41,15 @@ router.get("/:id", (req, res) => {
     .catch((err) => {
       res
         .status(500)
-        .json({ error: "Failed to get the lease", err: err.message });
+        .json({ error: "Failed to get the application", err: err.message });
     });
 });
 
 router.put("/:id", (req, res) => {
   const id = req.params.id;
-  const lease_term = req.body;
-  lease_term
-    .updateLeaseTerm(lease_term, id)
+  const application = req.body;
+  applications
+    .updateApp(application, id)
     .then((updated) => {
       if (updated) {
         res.status(200).json({ updated });
@@ -60,21 +60,23 @@ router.put("/:id", (req, res) => {
     .catch((err) => {
       res
         .status(500)
-        .json({ err: err.message, message: "Error updating lease term" });
+        .json({ err: err.message, message: "Error updating application" });
     });
 });
 
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
-  lease_term
-    .removeLeaseTerm(id)
+  applications
+    .removeApp(id)
     .then((nan) =>
-      res.status(204).json({ message: `Lease term id: ${id} has been deleted` })
+      res
+        .status(204)
+        .json({ message: `Application id: ${id} has been deleted` })
     )
     .catch((err) =>
       res
         .status(500)
-        .json({ error: "Failed to delete lease term", err: err.message })
+        .json({ error: "Failed to delete application", err: err.message })
     );
 });
 
